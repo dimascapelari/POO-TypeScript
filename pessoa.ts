aaae15b211326a1794c4d200a42fcb14cc8e374d
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-11 14:39:37 -0300

    POO TypeScript aula 01

---
+++ pessoa.ts
@@ -0,0 +1,12 @@
+export default class Pessoa {
+  constructor(n: string, i: number) {
+    this.nome = n;
+    this.idade = i;
+  }
+  nome: string;
+  idade: number;
+}
+
+let p1 = new Pessoa("Dimas", 40);
+
+console.log(`Nome: ${p1.nome}, Idade: ${p1.idade}`);
